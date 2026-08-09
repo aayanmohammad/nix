65c8912fa248398dc152dd00c751a97f1d81849d
Aayan Mohammad <271364631+aayanmohammad@users.noreply.github.com>
2026-08-09 14:56:11 +0600

    Include less pager

--- modules/git.nix
+++ modules/git.nix
@@ -3,7 +3,10 @@
 {
   programs.git.enable = true;
 
-  home.packages = with pkgs; [ openssh ];
+  home.packages = with pkgs; [
+    openssh
+    less
+  ];
 
   home.file.".gitconfig".source = ../configs/.gitconfig;
 }
